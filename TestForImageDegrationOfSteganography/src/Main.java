72fec56396ca0693395cefe8d01c0b0916ad1e35
sakuna63 <saku.ni63@gmail.com>
2013-10-18 09:23:38 +0900

    一旦コミット

--- TestForImageDegrationOfSteganography/src/Main.java
+++ TestForImageDegrationOfSteganography/src/Main.java
@@ -1,16 +1,80 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+import javax.imageio.ImageIO;
+
 public class Main {
+	private static final String IMAGE_PATH = "./img/";
+	private static final String CHARACTER_CODE = "US-ASCII";
+	private static final int TEXT_LENGTH = 256;
+	
 	public static void main(String[] args) {
-		String text = getRandomText(256);
-		System.out.println( text );
+//		String text = getRandomText(TEXT_LENGTH);
+//		System.out.println( text );
+		
+		File imgDir = new File(IMAGE_PATH);
+//		for(String name : imgDir.list()) {
+//			System.out.println(name);
+//		}
+		
+		FileInputStream in = null;
+		byte[] buff = new byte[30];
+		try {
+			in = new FileInputStream(imgDir.listFiles()[0]);
+			if( in != null ) {	
+				in.read(buff);
+				in.close();
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		print(new String(buff));
+		for(int k=0; k<buff.length; k++)
+			print(buff[k]);
+		
+		
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(imgDir.listFiles()[0]);
+//			img = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+//		print("pixel:" + img.getData().getNumDataElements());
+		
+		int argb, a, r, g, b;
+		for(int i=0; i<img.getHeight(); i++) {
+			for(int j=0; j<img.getWidth(); j++) {
+				argb = img.getRGB(i, j);
+				a = (argb>>>24) & 0xff;
+				r = (argb>>>16) & 0xff;
+				g = (argb>>> 8) & 0xff;
+				b = argb        & 0xff;
+//				System.out.println("argb:"+argb+"a:"+a+" r:"+r+" g:"+g+" b:"+b);
+//				print("pixel("+i+","+j+"):" + img.getRGB(i, j));
+			}
+		}
     }
+	
+	private static <T> void print(T text) {
+		System.out.println(text);
+	}
 
 	private static String getRandomText(int textNum) {
 		byte[] randByteArray = getRandomTextByte(textNum);
 		String text = null;
 		try {
-			text =  new String(randByteArray, "US-ASCII");
+			text =  new String(randByteArray, CHARACTER_CODE);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
