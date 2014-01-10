1c18f9aa50f3aec748ad4a76188ba674059bdea1
Ippei Nawate <saku.ni63@gmail.com>
2014-01-10 18:29:39 +0900

    いろいろいらないデータの削除と情報処理学会の予稿完成と最終発表の予稿作り始め

---
+++ documents/FinalReport/draft/convert.py
@@ -0,0 +1,9 @@
+import glob
+import cv2
+
+files = glob.glob("./img/*.bmp")
+
+for f in files:
+    img = cv2.imread(f, 0)
+    name = f.split("/")[-1].replace(".bmp", "")
+    cv2.imwrite("./img/" + name + ".png", img)    
